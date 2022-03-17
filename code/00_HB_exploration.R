f8d5aae3c07df1263c4ff39cac3c661950de4a08
Sz-Tim <tim.szewczyk@gmail.com>
2022-03-17 13:17:31 +0000

    taking real data

--- code/00_HB_exploration.R
+++ code/00_HB_exploration.R
@@ -39,7 +39,6 @@ fsa.df <- fromJSON(glue("data{sep}copy_fsa.txt")) %>%
          ydaySin=sin(yday/365),
          x_y=paste0(easting, "_", northing)) %>%
   filter(year > 2019 & year < 2022)
-fsa.sf <- st_as_sf(fsa.df, coords=c("easting", "northing"), crs=st_crs(27700))
 
 
 
@@ -49,8 +48,32 @@ fsa.sf <- st_as_sf(fsa.df, coords=c("easting", "northing"), crs=st_crs(27700))
 # simulation settings -----------------------------------------------------
 
 hydro.par <- setSimParams(nDays=29)
-site.xy <- fsa.df %>% group_by(sin, area, site) %>% 
-  summarise(lon=mean(easting), lat=mean(northing))
-sampling.df <- makeSimSamples(hydro.par, westcoms.dir, mesh.f, site.xy, sep)
+mesh.sf <- st_read(mesh.f)
+mesh <- loadMesh(mesh.f)
+sampling.df <- fsa.df %>%
+  mutate(dateChar=str_remove_all(date_collected, "-"),
+         time=rbeta(n(), 2, 5)*12+7,
+         hour=floor(time),
+         minutes=round(time %% 1 * 60),
+         mode=sample(hydro.par$modes, n(), T, c(0.7, 0.2, 0.1)),
+         depth=sample(hydro.par$depths, n(), T),
+         tides=sample(hydro.par$tides, n(), T, c(0.2, 0.05, 0.5, 0.05, 0.3))) %>%
+  st_as_sf(coords=c("easting", "northing"), remove=F, crs=27700) %>% 
+  st_join(., mesh.sf %>% select(i)) %>% 
+  filter(!is.na(i)) %>%
+  rename(site.elem=i) %>%
+  mutate(site.id=as.numeric(as.factor(site.elem)),
+         short_wave=NA, zeta=NA, temp=NA)
+site_trinode <- ncvar_get(mesh, "trinodes")[sampling.df$site.elem,]
+nc_close(mesh)
 
-
+# load relevant data
+sampling_dates <- unique(sampling.df$dateChar)
+for(i in 1:length(sampling_dates)) {
+  sampling.df <- sampling.df %>%
+    loadHydroVars(sampling_dates[i], site_trinode, westcoms.dir, sep)
+}
+sampling.df <- sampling.df %>%
+  mutate(across(one_of("time", "depth", "tides", "short_wave", "zeta", "temp"), 
+                CenterScale, 
+                .names="{.col}_sc"))
