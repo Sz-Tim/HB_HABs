3d4f96fccacb82a939772058a3f446cfe18d97d4
Sz-Tim <tim.szewczyk@gmail.com>
2022-03-29 14:36:42 +0100

    automatic minch2/westcoms2 selection

--- code/000_fnSimulations.R
+++ code/000_fnSimulations.R
@@ -15,19 +15,15 @@ setSimParams <- function(nSites=45, nDays=30,
 
 
 
-makeSimSamples <- function(par.ls, westcoms.dir=NULL, mesh.f=NULL, site.xy=NULL, 
-                           sep=sep, startDate="20190401", lnlambda_prior=c(7,2)) {
+makeSimSamples <- function(par.ls, westcoms.dir=NULL, mesh.f=NULL,
+                           obs.df, sep=sep, lnlambda_prior=c(7,2)) {
   library(tidyverse); library(lubridate); library(glue); library(LaplacesDemon)
   
   # generate sampling details and lnlambdas
-  sampling.df <- tibble(sampleID=1:(par.ls$N),
-                        site=rep(1:par.ls$nSites, times=par.ls$nDays),
-                        day=rep(0:(par.ls$nDays-1), each=par.ls$nSites)) %>%
-    mutate(date=ymd(startDate)+day,
+  sampling.df <- obs.df %>% 
+    mutate(date=date_collected,
            dateChar=str_remove_all(date, "-"),
-           time=rbeta(n(), 2, 5)*12+7,
-           hour=floor(time),
-           minutes=round(time %% 1 * 60),
+           time=hour + minutes/60,
            mode=sample(par.ls$modes, n(), T, c(0.7, 0.2, 0.1)),
            depth=sample(par.ls$depths, n(), T),
            tides=sample(par.ls$tides, n(), T, c(0.2, 0.05, 0.5, 0.05, 0.3)),
@@ -35,28 +31,38 @@ makeSimSamples <- function(par.ls, westcoms.dir=NULL, mesh.f=NULL, site.xy=NULL,
            lnlambdap1=log(exp(lnlambda)+1))
 
   # extract element IDs, trinodes for each observation
-  mesh.sf <- st_read(mesh.f)
-  mesh <- loadMesh(mesh.f)
-  site.xy <- site.xy %>% 
-    st_as_sf(coords=c("lon", "lat"), remove=F, crs=27700) %>% 
-    st_join(., mesh.sf) %>% 
-    filter(!is.na(i))
+  mesh.sf <- map(mesh.f, st_read)
+  mesh <- map(mesh.f, loadMesh)
+  site.xy <- obs.df %>% select("grid", "site.id", "lon", "lat") %>%
+    group_by(grid, site.id) %>% slice_head(n=1) %>%
+    group_by(grid) %>%
+    group_split() %>%
+    map2(.x=., .y=mesh.sf, 
+         ~st_as_sf(.x, coords=c("lon", "lat"), remove=F, crs=27700) %>% 
+           st_join(., .y %>% 
+                     select(-area) %>% 
+                     rename(depth.elem=depth,
+                            site.elem=i)) %>% 
+           filter(!is.na(site.elem)) %>%
+           st_drop_geometry) %>%
+    bind_rows
   sampling.df <- sampling.df %>%
-    mutate(site.id=sample(1:nrow(site.xy), n(), replace=T),
-           site.lat=site.xy$lat[site.id],
-           site.lon=site.xy$lon[site.id],
-           site.elem=site.xy$i[site.id],
-           short_wave=NA, zeta=NA, temp=NA)
-  site_trinode <- ncvar_get(mesh, "trinodes")[sampling.df$site.elem,]
-  nc_close(mesh)
-  
+    right_join(., site.xy, by=c("grid", "site.id", "lon", "lat")) %>%
+    group_by(grid) %>% group_split
+  site_trinode <- map2(.x=mesh, .y=sampling.df,
+                       ~ncvar_get(.x, "trinodes")[.y$site.elem,])
+  walk(mesh, nc_close)
+    
   # load relevant data
-  sampling_dates <- unique(sampling.df$dateChar)
-  for(i in 1:length(sampling_dates)) {
-    sampling.df <- sampling.df %>%
-      loadHydroVars(sampling_dates[i], site_trinode, westcoms.dir, sep)
+  for(grid in 1:2) {
+    sampling_dates <- unique(sampling.df[[grid]]$dateChar)
+    for(i in 1:length(sampling_dates)) {
+      sampling.df[[grid]] <- sampling.df[[grid]] %>%
+        loadHydroVars(sampling_dates[i], site_trinode[[grid]], westcoms.dir[[grid]], sep)
+    }
   }
   sampling.df <- sampling.df %>%
+    bind_rows %>%
     mutate(across(one_of("time", "depth", "tides", "short_wave", "zeta", "temp"), 
                   CenterScale, 
                   .names="{.col}_sc"))
--- code/00_HB_exploration.R
+++ code/00_HB_exploration.R
@@ -9,19 +9,25 @@
 
 # set up ------------------------------------------------------------------
 
-pkgs <- c("tidyverse", "lubridate", "glue", "ncdf4", "sf",
+pkgs <- c("tidyverse", "lubridate", "glue", "ncdf4", "sf", "LaplacesDemon",
           "cmdstanr", "bayesplot", "posterior", "jsonlite")
 suppressMessages(invisible(lapply(pkgs, library, character.only=T)))
 theme_set(theme_bw() + theme(panel.grid.minor=element_blank()))
 walk(dir("code", "00[0-9]_fn", full.names=T), source)
-sep <- ifelse(.Platform$OS.type=="unix", "/", "\\")
 
-westcoms.dir <- ifelse(.Platform$OS.type=="unix",
-                       "/media/archiver/common/sa01da-work/WeStCOMS2/Archive/",
-                       "D:\\hydroOut\\WestCOMS2\\Archive\\")
-mesh.f <- ifelse(.Platform$OS.type=="unix",
-                 "/home/sa04ts/FVCOM_meshes/WeStCOMS2_mesh.gpkg",
-                 "..\\..\\01_FVCOM\\data\\WeStCOMS2_mesh.gpkg")
+# minch2:    2013-06-20 to 2019-07-02
+# WeStCOMS2: 2019-04-01 to 2022-01-26
+if(.Platform$OS.type=="unix") {
+  sep <- "/"
+  westcoms.dir <- c("/media/archiver/common/sa01da-work/WeStCOMS2/Archive/")
+  mesh.f <- c("/home/sa04ts/FVCOM_meshes/WeStCOMS2_Mesh.gpkg")
+} else {
+  sep <- "\\"
+  westcoms.dir <- c("D:\\hydroOut\\minch2\\Archive\\",
+                    "D:\\hydroOut\\WestCOMS2\\Archive\\")
+  mesh.f <- c("..\\..\\01_FVCOM\\data\\WeStCOMS_Mesh.gpkg",
+              "..\\..\\01_FVCOM\\data\\WeStCOMS2_Mesh.gpkg")
+}
 
 
 fsa.df <- fromJSON(glue("data{sep}copy_fsa.txt")) %>% 
@@ -31,49 +37,110 @@ fsa.df <- fromJSON(glue("data{sep}copy_fsa.txt")) %>%
   filter(easting > 0 & northing > 0) %>%
   filter(!is.na(date_collected)) %>%
   filter(karenia_mikimotoi >= 0) %>% # -99 in karenia...?
-  mutate(date_collected=date(date_collected),
+  mutate(datetime_collected=as_datetime(date_collected),
+         date_collected=date(datetime_collected),
          year=year(date_collected),
          month=month(date_collected),
-         yday=yday(date_collected),
-         ydayCos=cos(yday/365),
-         ydaySin=sin(yday/365),
-         x_y=paste0(easting, "_", northing)) %>%
-  filter(year > 2019 & year < 2022)
+         hour=hour(datetime_collected),
+         minutes=minute(datetime_collected),
+         grid=if_else(date_collected < "2019-04-01", "minch2", "WeStCOMS2")) %>%
+  filter(datetime_collected >= "2013-06-20") %>% 
+  group_by(sin, area, site) %>% 
+  mutate(lon=mean(easting), lat=mean(northing)) %>%
+  ungroup %>% 
+  mutate(site.id=as.numeric(factor(paste(sin, area, site))))
 
 
 
 
 
 
-# simulation settings -----------------------------------------------------
 
-hydro.par <- setSimParams(nDays=29)
-mesh.sf <- st_read(mesh.f)
-mesh <- loadMesh(mesh.f)
-sampling.df <- fsa.df %>%
-  mutate(dateChar=str_remove_all(date_collected, "-"),
-         time=rbeta(n(), 2, 5)*12+7,
-         hour=floor(time),
-         minutes=round(time %% 1 * 60),
-         mode=sample(hydro.par$modes, n(), T, c(0.7, 0.2, 0.1)),
-         depth=sample(hydro.par$depths, n(), T),
-         tides=sample(hydro.par$tides, n(), T, c(0.2, 0.05, 0.5, 0.05, 0.3))) %>%
-  st_as_sf(coords=c("easting", "northing"), remove=F, crs=27700) %>% 
-  st_join(., mesh.sf %>% select(i)) %>% 
-  filter(!is.na(i)) %>%
-  rename(site.elem=i) %>%
-  mutate(site.id=as.numeric(as.factor(site.elem)),
-         short_wave=NA, zeta=NA, temp=NA)
-site_trinode <- ncvar_get(mesh, "trinodes")[sampling.df$site.elem,]
-nc_close(mesh)
+# munge datasets ----------------------------------------------------------
+
+par.ls <- setSimParams()
+# generate sampling details and lnlambdas
+sampling.df <- obs.df %>% 
+  mutate(date=date_collected,
+         dateChar=str_remove_all(date, "-"),
+         time=hour + minutes/60,
+         mode=sample(par.ls$modes, n(), T, c(0.7, 0.2, 0.1)),
+         depth=sample(par.ls$depths, n(), T),
+         tides=sample(par.ls$tides, n(), T, c(0.2, 0.05, 0.5, 0.05, 0.3)),
+         lnlambda=rnorm(n(), lnlambda_prior[1], lnlambda_prior[2]),
+         lnlambdap1=log(exp(lnlambda)+1))
+
+# extract element IDs, trinodes for each observation
+mesh.sf <- map(mesh.f, st_read)
+mesh <- map(mesh.f, loadMesh)
+site.xy <- obs.df %>% select("grid", "site.id", "lon", "lat") %>%
+  group_by(grid, site.id) %>% slice_head(n=1) %>%
+  group_by(grid) %>%
+  group_split() %>%
+  map2(.x=., .y=mesh.sf, 
+       ~st_as_sf(.x, coords=c("lon", "lat"), remove=F, crs=27700) %>% 
+         st_join(., .y %>% 
+                   select(-area) %>% 
+                   rename(depth.elem=depth,
+                          site.elem=i)) %>% 
+         filter(!is.na(site.elem)) %>%
+         st_drop_geometry) %>%
+  bind_rows
+sampling.df <- sampling.df %>%
+  right_join(., site.xy, by=c("grid", "site.id", "lon", "lat")) %>%
+  group_by(grid) %>% group_split
+site_trinode <- map2(.x=mesh, .y=sampling.df,
+                     ~ncvar_get(.x, "trinodes")[.y$site.elem,])
+walk(mesh, nc_close)
 
 # load relevant data
-sampling_dates <- unique(sampling.df$dateChar)
-for(i in 1:length(sampling_dates)) {
-  sampling.df <- sampling.df %>%
-    loadHydroVars(sampling_dates[i], site_trinode, westcoms.dir, sep)
+for(grid in 1:2) {
+  sampling_dates <- unique(sampling.df[[grid]]$dateChar)
+  for(i in 1:length(sampling_dates)) {
+    sampling.df[[grid]] <- sampling.df[[grid]] %>%
+      loadHydroVars(sampling_dates[i], site_trinode[[grid]], westcoms.dir[[grid]], sep)
+  }
 }
 sampling.df <- sampling.df %>%
+  bind_rows %>%
   mutate(across(one_of("time", "depth", "tides", "short_wave", "zeta", "temp"), 
                 CenterScale, 
                 .names="{.col}_sc"))
+
+write_csv(sampling.df, glue("data{sep}obs_df.csv"))
+
+
+
+
+# prepare model -----------------------------------------------------------
+
+samp.mx <- model.matrix(~ 0 + time_sc + I(time_sc^2) + 
+                          short_wave_sc + zeta_sc + temp_sc + mode, 
+                        data=sampling.df)
+
+mod <- cmdstan_model(glue("models{sep}00_sampling_simple.stan"))
+sampling_hydro.data <- list(
+  N=nrow(sampling.df),
+  X=samp.mx,
+  nCov=ncol(samp.mx),
+  y=round(sampling.df$pseudo_nitzschia_sp)
+)
+
+
+
+
+
+
+# run model ---------------------------------------------------------------
+
+fit <- mod$sample(data=sampling_hydro.data,
+                  chains=4, parallel_chains=4,
+                  max_treedepth=20,
+                  refresh=500, iter_sampling=2000)
+fit$cmdstan_diagnose()
+out.sum <- fit$summary()
+
+# I think these values are too large for a poisson distribution and that I
+# should consider alternatives. For example, a hurdle model with a normal
+# distribution using log-transformed values might be an ok solution.
+
