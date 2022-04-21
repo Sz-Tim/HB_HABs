c2b9446308383e710a8a0709e75ec062823f8f5a
Sz-Tim <tim.szewczyk@gmail.com>
2022-04-21 09:57:39 +0100

    parallelized species, added options for category predictions

--- code/03_HB_weekPred.R
+++ code/03_HB_weekPred.R
@@ -9,10 +9,15 @@
 
 # set up ------------------------------------------------------------------
 
-pkgs <- c("tidyverse", "lubridate", "glue", "LaplacesDemon", "brms", "bayesplot", "WeStCOMS")
+pkgs <- c("tidyverse", "lubridate", "glue", "LaplacesDemon", "brms", "WeStCOMS",
+          "foreach", "doSNOW")
 suppressMessages(invisible(lapply(pkgs, library, character.only=T)))
 theme_set(theme_bw() + theme(panel.grid.minor=element_blank()))
 walk(dir("code", "000_fn", full.names=T), source)
+
+huf <- T
+ord <- T
+cores.sp <- 5
 
 # minch2:    2013-06-20 to 2019-07-02
 # WeStCOMS2: 2019-04-01 to 2022-01-26
@@ -48,7 +53,7 @@ thresh.df <- read_csv(glue("data{sep}hab_tf_thresholds.csv")) %>%
   ungroup
 
 hydro.df <- dir("data", "hydro_", full.names=T) %>% 
-  map(read_csv) %>%
+  map(~read_csv(.x, show_col_types=F)) %>%
   reduce(., full_join) %>% 
   pivot_longer(c(contains("_L"), contains("_R")), names_to="param", values_to="value") %>%
   mutate(parType=str_sub(param, 1, -4),
@@ -137,7 +142,9 @@ sampling.df <- sampling.df %>%
          month=month(date))
 weeks <- sort(unique(filter(sampling.df, year(date)>2017 & between(month, 3, 11))$wk))
 
-for(sp in 1:length(species)) {
+foreach(sp=1:length(species), 
+        .packages=pkgs,
+        .errorhandling="remove") %dopar% {
   target <- species[sp]
   target.tf <- thresh.df %>% filter(hab_parameter==target)
   out.ord <- out.huf <- vector("list", length(weeks))
@@ -180,75 +187,87 @@ for(sp in 1:length(species)) {
     
     if(i == 1) {
       train.df <- target.df %>% filter(wk <= week_fit)
-      out_ord_im1 <- readRDS(glue("out{sep}initFit{sep}ordinal_{target}.rds")) 
-      out_huf_im1 <- readRDS(glue("out{sep}initFit{sep}huf_{target}.rds")) 
+      if(ord) {
+        out_ord_im1 <- readRDS(glue("out{sep}initFit{sep}ordinal_{target}.rds")) 
+      }
+      if(huf) {
+        out_huf_im1 <- readRDS(glue("out{sep}initFit{sep}huf_{target}.rds")) 
+      }
     } else {
       train.df <- target.df %>% filter(wk == week_fit)
-      out_ord_im1 <- readRDS(glue("out{sep}weekFit{sep}ordinal_{target}_{weeks[i-1]}.rds")) 
-      out_huf_im1 <- readRDS(glue("out{sep}weekFit{sep}huf_{target}_{weeks[i-1]}.rds"))  
+      if(ord) {
+        out_ord_im1 <- readRDS(glue("out{sep}weekFit{sep}ordinal_{target}_{weeks[i-1]}.rds"))  
+      }
+      if(huf) {
+        out_huf_im1 <- readRDS(glue("out{sep}weekFit{sep}huf_{target}_{weeks[i-1]}.rds"))  
+      }
     }
     test.df <- target.df %>% filter(wk == week_pred)
+    
     # ordinal priors
-    b_ord_im1 <- as_draws_df(out_ord_im1, variable="b_", regex=T) %>%
-      pivot_longer(cols=starts_with("b_"), names_to="param", values_to="val") %>%
-      group_by(param) %>%
-      summarise(mn=mean(val), sd=sd(val)) %>%
-      mutate(coef=str_remove(param, "b_"))
-    eff_ord_im1 <- b_ord_im1 %>% filter(!grepl("Intercept", param))
-    int_ord_im1 <- b_ord_im1 %>% filter(grepl("Intercept", param)) %>%
-      mutate(coef=str_remove(str_remove(str_remove(coef, "Intercept"), "\\["), "]"))
-    sd_im1 <- summary(out_ord_im1)$random$site.id
-    prior_ord.ls <- vector("list", nrow(b_ord_im1)+1)
-    for(j in 1:nrow(int_ord_im1)) {
-      prior_ord.ls[[j]] <- prior_string(glue("normal({int_ord_im1$mn[j]},{int_ord_im1$sd[j]})"),
-                                    class="Intercept", coef=int_ord_im1$coef[j])
+    if(ord) {
+      b_ord_im1 <- as_draws_df(out_ord_im1, variable="b_", regex=T) %>%
+        pivot_longer(cols=starts_with("b_"), names_to="param", values_to="val") %>%
+        group_by(param) %>%
+        summarise(mn=mean(val), sd=sd(val)) %>%
+        mutate(coef=str_remove(param, "b_"))
+      eff_ord_im1 <- b_ord_im1 %>% filter(!grepl("Intercept", param))
+      int_ord_im1 <- b_ord_im1 %>% filter(grepl("Intercept", param)) %>%
+        mutate(coef=str_remove(str_remove(str_remove(coef, "Intercept"), "\\["), "]"))
+      sd_im1 <- summary(out_ord_im1)$random$site.id
+      prior_ord.ls <- vector("list", nrow(b_ord_im1)+1)
+      for(j in 1:nrow(int_ord_im1)) {
+        prior_ord.ls[[j]] <- prior_string(glue("normal({int_ord_im1$mn[j]},{int_ord_im1$sd[j]})"),
+                                          class="Intercept", coef=int_ord_im1$coef[j])
+      }
+      for(j in 1:nrow(eff_ord_im1)) {
+        prior_ord.ls[[j+nrow(int_ord_im1)]] <- prior_string(glue("normal({eff_ord_im1$mn[j]},{eff_ord_im1$sd[j]})"),
+                                                            class="b", coef=eff_ord_im1$coef[j])
+      }
+      prior_ord.ls[[nrow(b_ord_im1)+1]] <- prior_string(glue("normal({sd_im1$Estimate},{sqrt(sd_im1$Est.Error)})"),
+                                                        class="sd")
+      prior_ord <- do.call(rbind, prior_ord.ls)
     }
-    for(j in 1:nrow(eff_ord_im1)) {
-      prior_ord.ls[[j+nrow(int_ord_im1)]] <- prior_string(glue("normal({eff_ord_im1$mn[j]},{eff_ord_im1$sd[j]})"),
-                                                  class="b", coef=eff_ord_im1$coef[j])
-    }
-    prior_ord.ls[[nrow(b_ord_im1)+1]] <- prior_string(glue("normal({sd_im1$Estimate},{sqrt(sd_im1$Est.Error)})"),
-                                              class="sd")
-    prior_ord <- do.call(rbind, prior_ord.ls)
     
     # hurdle frechet priors
-    b_huf_im1 <- as_draws_df(out_huf_im1, variable="b_", regex=T) %>%
-      pivot_longer(cols=starts_with("b_"), names_to="param", values_to="val") %>%
-      group_by(param) %>%
-      summarise(mn=mean(val), sd=sd(val)) %>%
-      mutate(coef=str_remove(param, "b_"))
-    fr_int_huf_im1 <- b_huf_im1 %>% filter(grepl("Intercept", param) & !grepl("_hu_", param)) %>%
-      mutate(coef="")
-    hu_int_huf_im1 <- b_huf_im1 %>% filter(grepl("Intercept", param) & grepl("_hu_", param)) %>%
-      mutate(coef="")
-    fr_eff_huf_im1 <- b_huf_im1 %>% filter(!grepl("Intercept", param) & !grepl("_hu_", param))
-    hu_eff_huf_im1 <- b_huf_im1 %>% filter(!grepl("Intercept", param) & grepl("_hu_", param)) %>%
-      mutate(coef=str_remove(coef, "hu_"))
-    sd_im1 <- summary(out_huf_im1)$random$site.id
-    nu_im1 <- summary(out_huf_im1)$spec_pars
-    prior_huf.ls <- vector("list", nrow(b_huf_im1))
-    prior_huf.ls[[1]] <- prior_string(glue("normal({fr_int_huf_im1$mn[1]},{fr_int_huf_im1$sd[1]})"),
-                                      class="Intercept", coef="")
-    prior_huf.ls[[2]] <- prior_string(glue("normal({hu_int_huf_im1$mn[1]},{hu_int_huf_im1$sd[1]})"),
-                                      class="Intercept", coef="", dpar="hu")
-    k <- 3
-    for(j in 1:nrow(fr_eff_huf_im1)) {
-      prior_huf.ls[[k]] <- prior_string(glue("normal({fr_eff_huf_im1$mn[j]},{fr_eff_huf_im1$sd[j]})"),
+    if(huf) {
+      b_huf_im1 <- as_draws_df(out_huf_im1, variable="b_", regex=T) %>%
+        pivot_longer(cols=starts_with("b_"), names_to="param", values_to="val") %>%
+        group_by(param) %>%
+        summarise(mn=mean(val), sd=sd(val)) %>%
+        mutate(coef=str_remove(param, "b_"))
+      fr_int_huf_im1 <- b_huf_im1 %>% filter(grepl("Intercept", param) & !grepl("_hu_", param)) %>%
+        mutate(coef="")
+      hu_int_huf_im1 <- b_huf_im1 %>% filter(grepl("Intercept", param) & grepl("_hu_", param)) %>%
+        mutate(coef="")
+      fr_eff_huf_im1 <- b_huf_im1 %>% filter(!grepl("Intercept", param) & !grepl("_hu_", param))
+      hu_eff_huf_im1 <- b_huf_im1 %>% filter(!grepl("Intercept", param) & grepl("_hu_", param)) %>%
+        mutate(coef=str_remove(coef, "hu_"))
+      sd_im1 <- summary(out_huf_im1)$random$site.id
+      nu_im1 <- summary(out_huf_im1)$spec_pars
+      prior_huf.ls <- vector("list", nrow(b_huf_im1))
+      prior_huf.ls[[1]] <- prior_string(glue("normal({fr_int_huf_im1$mn[1]},{fr_int_huf_im1$sd[1]})"),
+                                        class="Intercept", coef="")
+      prior_huf.ls[[2]] <- prior_string(glue("normal({hu_int_huf_im1$mn[1]},{hu_int_huf_im1$sd[1]})"),
+                                        class="Intercept", coef="", dpar="hu")
+      k <- 3
+      for(j in 1:nrow(fr_eff_huf_im1)) {
+        prior_huf.ls[[k]] <- prior_string(glue("normal({fr_eff_huf_im1$mn[j]},{fr_eff_huf_im1$sd[j]})"),
                                           class="b", coef=fr_eff_huf_im1$coef[j])
-      k <- k+1
+        k <- k+1
+      }
+      for(j in 1:nrow(hu_eff_huf_im1)) {
+        prior_huf.ls[[k]] <- prior_string(glue("normal({hu_eff_huf_im1$mn[j]},{hu_eff_huf_im1$sd[j]})"),
+                                          class="b", coef=hu_eff_huf_im1$coef[j], dpar="hu")
+        k <- k+1
+      }
+      prior_huf.ls[[k]] <- prior_string(glue("normal({sd_im1$Estimate},{sqrt(sd_im1$Est.Error)})"),
+                                        class="sd")
+      prior_huf.ls[[k+1]] <- prior_string(glue("normal({nu_im1$Estimate},{sqrt(nu_im1$Est.Error)})"),
+                                          class="nu")
+      
+      prior_huf <- do.call(rbind, prior_huf.ls)
     }
-    for(j in 1:nrow(hu_eff_huf_im1)) {
-      prior_huf.ls[[k]] <- prior_string(glue("normal({hu_eff_huf_im1$mn[j]},{hu_eff_huf_im1$sd[j]})"),
-                                        class="b", coef=hu_eff_huf_im1$coef[j], dpar="hu")
-      k <- k+1
-    }
-    prior_huf.ls[[k]] <- prior_string(glue("normal({sd_im1$Estimate},{sqrt(sd_im1$Est.Error)})"),
-                                      class="sd")
-    prior_huf.ls[[k+1]] <- prior_string(glue("normal({nu_im1$Estimate},{sqrt(nu_im1$Est.Error)})"),
-                                        class="nu")
-    
-    prior_huf <- do.call(rbind, prior_huf.ls)
-    
     
     
     date_range <- paste(as.character(range(train.df$date)), collapse=" to ")
@@ -256,49 +275,86 @@ for(sp in 1:length(species)) {
     cat("  train:", nrow(train.df), "rows with", n_distinct(train.df$site.id), "sites",
         "    ", table(train.df$N.catF), "\n")
     if(i == 1) {
-      stanvars <- stanvar(scode=readr::read_file(glue("models{sep}hurdle_frechet_fn.stan")),
-                          block="functions")
-      out.ord[[i]] <- brm(form_ordinal, train.df, cumulative("probit"),
-                      prior=prior_ord, chains=4, cores=4, 
-                      iter=3000, warmup=2000, refresh=100, inits="0",
-                      control=list(adapt_delta=0.95, max_treedepth=20),
-                      file=glue("out{sep}weekFit{sep}ordinal_{target}_{week_fit}"))
-      out.huf[[i]] <- brm(form_huf, train.df, hurdle_frechet,
-                          prior=prior_huf, chains=4, cores=4, 
-                          iter=3000, warmup=2000, refresh=100, inits="0",
-                          control=list(adapt_delta=0.95, max_treedepth=20),
-                          stanvars=stanvars,
-                          file=glue("out{sep}weekFit{sep}huf_{target}_{week_fit}"))
+      if(ord) {
+        out.ord[[i]] <- brm(form_ordinal, train.df, cumulative("probit"),
+                            prior=prior_ord, chains=4, cores=4, 
+                            iter=3000, warmup=2000, refresh=100, inits="0",
+                            control=list(adapt_delta=0.95, max_treedepth=20),
+                            file=glue("out{sep}weekFit{sep}ordinal_{target}_{week_fit}"))
+      }
+      if(huf) {
+        stanvars <- stanvar(scode=readr::read_file(glue("models{sep}hurdle_frechet_fn.stan")),
+                            block="functions")
+        out.huf[[i]] <- brm(form_huf, train.df, hurdle_frechet,
+                            prior=prior_huf, chains=4, cores=4, 
+                            iter=3000, warmup=2000, refresh=100, inits="0",
+                            control=list(adapt_delta=0.95, max_treedepth=20),
+                            stanvars=stanvars,
+                            file=glue("out{sep}weekFit{sep}huf_{target}_{week_fit}")) 
+      }
     } else {
-      out.ord[[i]] <- update(out_ord_im1, newdata=train.df, cores=4, 
-                         prior=prior_ord, file=glue("out{sep}weekFit{sep}ordinal_{target}_{week_fit}")) 
-      out.huf[[i]] <- update(out_huf_im1, newdata=train.df, cores=4,
-                             prior=prior_huf, file=glue("out{sep}weekFit{sep}huf_{target}_{week_fit}")) 
+      if(ord) {
+        out.ord[[i]] <- update(out_ord_im1, newdata=train.df, cores=4, 
+                               prior=prior_ord, file=glue("out{sep}weekFit{sep}ordinal_{target}_{week_fit}")) 
+      }
+      if(huf) {
+        out.huf[[i]] <- update(out_huf_im1, newdata=train.df, cores=4,
+                               prior=prior_huf, file=glue("out{sep}weekFit{sep}huf_{target}_{week_fit}")) 
+      }
     }
     
-    pred.ord <- posterior_epred(out.ord[[i]], newdata=test.df, allow_new_levels=T)
-    pred.huf <- posterior_epred(out.huf[[i]], newdata=test.df, allow_new_levels=T)
-    
-    test.df %>% 
-      select(obs.id) %>%
-      mutate(huf_mn=colMeans(pred.huf),
-             huf_md=apply(pred.huf, 2, median),
-             huf_0=apply(exp(pred.huf)-1, 2,
-                         function(x) mean(x>=target.tf$min_ge[1] &
-                                            x<=target.tf$min_ge[2])),
-             huf_1=apply(exp(pred.huf)-1, 2,
-                         function(x) mean(x>=target.tf$min_ge[2] &
-                                            x<=target.tf$min_ge[3])),
-             huf_2=apply(exp(pred.huf)-1, 2,
-                         function(x) mean(x>=target.tf$min_ge[3] &
-                                            x<=target.tf$min_ge[4])),
-             huf_3=apply(exp(pred.huf)-1, 2,
-                         function(x) mean(x>=target.tf$min_ge[4])),
-             ord_0=colMeans(pred.ord[,,1]),
-             ord_1=colMeans(pred.ord[,,2]),
-             ord_2=colMeans(pred.ord[,,3]),
-             ord_3=colMeans(pred.ord[,,4])) %>%
-      write_csv(glue("out{sep}weekFit{sep}pred_{target}_{week_pred}.csv"))
+    pred.df <- test.df %>% select(obs.id)
+    if(ord) {
+      pred.ord <- posterior_epred(out.ord[[i]], newdata=test.df, allow_new_levels=T)
+      cat.iter <- apply(pred.ord, 1:2, which.max)
+      pred.df <- pred.df %>%
+        mutate(ord_mnpr0=colMeans(pred.ord[,,1]),
+               ord_mnpr1=colMeans(pred.ord[,,2]),
+               ord_mnpr2=colMeans(pred.ord[,,3]),
+               ord_mnpr3=colMeans(pred.ord[,,4]),
+               ord_prmax0=colMeans(cat.iter==1),
+               ord_prmax1=colMeans(cat.iter==2),
+               ord_prmax2=colMeans(cat.iter==3),
+               ord_prmax3=colMeans(cat.iter==4),
+               ord_mnCat=colMeans(cat.iter),
+               ord_wtmnCat=ord_mnpr0 + 2*ord_mnpr1 + 3*ord_mnpr2 + 4*ord_mnpr3)
+    }
+    if(huf) {
+      pred.hu.huf <- posterior_epred(out.huf[[i]], newdata=test.df, allow_new_levels=T, dpar="hu")
+      pred.mu.huf <- posterior_epred(out.huf[[i]], newdata=test.df, allow_new_levels=T, dpar="mu")
+      pred.huf <- (1-rbinom(prod(dim(pred.hu.huf)), 1, pred.hu.huf)) * pred.mu.huf
+      pred.huf.exp <- (1-rbinom(prod(dim(pred.hu.huf)), 1, pred.hu.huf)) * exp(pred.mu.huf)
+      cat.iter <- apply(pred.huf.exp, 1:2, function(x) (1:4)[max(which(x >= target.tf$min_ge))])
+      pred.df <- pred.df %>%
+        mutate(huf_mn=colMeans(pred.huf),
+               huf_mn_exp=colMeans(pred.huf.exp),
+               huf_md=apply(pred.huf, 2, median),
+               huf_md_exp=apply(pred.huf.exp, 2, median),
+               huf_mnpr0=apply(pred.huf.exp, 2,
+                           function(x) mean(x>=target.tf$min_ge[1] &
+                                              x<=target.tf$min_ge[2])),
+               huf_mnpr1=apply(pred.huf.exp, 2,
+                           function(x) mean(x>=target.tf$min_ge[2] &
+                                              x<=target.tf$min_ge[3])),
+               huf_mnpr2=apply(pred.huf.exp, 2,
+                           function(x) mean(x>=target.tf$min_ge[3] &
+                                              x<=target.tf$min_ge[4])),
+               huf_mnpr3=apply(pred.huf.exp, 2,
+                           function(x) mean(x>=target.tf$min_ge[4])),
+               huf_prmax0=colMeans(cat.iter==1),
+               huf_prmax1=colMeans(cat.iter==2),
+               huf_prmax2=colMeans(cat.iter==3),
+               huf_prmax3=colMeans(cat.iter==4),
+               huf_mnCat=colMeans(cat.iter),
+               huf_wtmnCat=huf_mnpr0 + 2*huf_mnpr1 + 3*huf_mnpr2 + 4*huf_mnpr3)
+    }
+    if(ord & huf) {
+      write_csv(pred.df, glue("out{sep}weekFit{sep}pred_{target}_{week_pred}.csv"))
+    } else if(ord) {
+      write_csv(pred.df, glue("out{sep}weekFit{sep}pred_ord_{target}_{week_pred}.csv"))
+    } else {
+      write_csv(pred.df, glue("out{sep}weekFit{sep}pred_huf_{target}_{week_pred}.csv"))
+    }
     
     cat("  Fitted model \n")
   }
