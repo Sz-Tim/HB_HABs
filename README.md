2e34b10f0603eecacebe29f643f5fd0b949bf6e3
Sz-Tim <tim.szewczyk@gmail.com>
2022-06-28 09:45:10 +0100

    updated file organisation for operational runs

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # HB_HABs
 Hierarchical Bayesian model for forecasting harmful algal blooms in Scotland
+
+These models aim to predict HABs and their associated toxins. There are currently four models (Hierarchical Bayesian cumulative ordinal, Hierarchical Bayesian dual logistic, Random Forest, and Random Forest split) along with an ensemble model. Each uses seasonally-varying effects, including autoregressive (2) terms, environmental conditions, particle tracking simulations of offshore influx, with site-level random effects. 
+
+The ultimate goal is to produce an operational model that generates forecasts automatically based on the new monitoring counts and the WeStCOMS model outputs as they become available.
+
+This involves the following steps:
+
+## Set up
+### code/01_initial_setup.R
+  1. Create particle tracking directories, properties file
+  2. Run particle tracking simulations
+  3. Compile particle tracking output
+  4. Extract WeStCOMS variables
+  5. Extract Copernicus variables
+  6. Extract algal densities
+  7. Extract toxin concentrations
+  8. Load traffic light thresholds
+  9. Compile datasets for each species
+### code/02-1_fit_initial_models.R
+  1. Load compiled datasets
+  2. Define predictors
+  3. for(year in years) { fit(data[-year]); predict(data[year])}
+### (code/02-2_variable_selection.R)
+  1. Load compiled datasets
+  2. Define sets of predictors
+  3. for(preds in predSets) { for(year in years) { ... } }
+### code/03_calc_model_weights.R
+  1. Load predictions
+  2. Calculate overall predictive likelihood
+  3. Calculate per-site predictive likelihood
+  4. Calculate per-month predictive likelihood
+
+## Weekly updates
+### code/04_weekly_main.R
+  - runs each of the following scripts in succession
+### code/04-1_get_new_data.R
+  1. Set dates
+  2. Download WeStCOMS (days -7:5)
+  3. Download Copernicus (days -7:5)
+  4. Download new algal densities
+  5. Download new toxin concentrations
+### code/04-2_simulate_particles.R
+  1. Create particle tracking directories, properties file
+  2. Copy previous particle locations file
+  3. Run particle tracking simulations with hot start
+### code/04-3_compile_data.R
+  1. Compile particle tracking output
+  2. Extract and compile WeStCOMS, Copernicus
+  3. Append new days to previous dataset, replacing with new data as needed
+  4. Compile datasets for each species
+### code/04-4_fit_new_models.R
+  1. Load compiled datasets
+  2. Define predictors
+  3. Fit models with observed data
+  4. Predict probabilities for the next week
+### code/04-5_export_predictions.R
+  1. Load predictions
+  2. Calculate ensemble predictions
+  3. Clean and summarise
+  4. Export to csv, map, etc 
---
+++ code/deprecated/0000_tempCodeToOrganize.R
@@ -0,0 +1,211 @@
+# CODE THAT NEEDS TO BE ORGANIZED
+
+
+
+# get fetch ---------------------------------------------------------------
+
+library(raster); library(sf)
+fetch <- raster("..\\..\\00_gis\\fetch\\log10_UK200m_depth40_0na.tif")
+sites.sf <- read_csv("data\\sampling_local.csv") %>% group_by(site.id) %>% 
+  summarise(lon=first(lon), lat=first(lat)) %>%
+  st_as_sf(coords=c("lon", "lat"), crs=27700) %>%
+  mutate(fetch=raster::extract(fetch, ., fun=mean, small=T))
+read_csv("data\\sampling_local.csv") %>% left_join(st_drop_geometry(sites.sf)) %>%
+  write_csv("data\\sampling_local.csv")
+
+sites.sf <- read_csv("data\\sampling_regional.csv") %>% group_by(site.id) %>% 
+  summarise(lon=first(lon), lat=first(lat)) %>%
+  st_as_sf(coords=c("lon", "lat"), crs=27700) %>%
+  mutate(fetch=raster::extract(fetch, ., fun=mean, small=T))
+read_csv("data\\sampling_regional.csv") %>% left_join(st_drop_geometry(sites.sf)) %>%
+  write_csv("data\\sampling_regional.csv")
+
+
+
+
+
+library(tidyverse); library(sf)
+
+mesh <- st_read("..\\..\\WeStCOMS\\data\\WeStCOMS2_meshOutline.gpkg")
+fsa.pt <- st_read("data\\fsa_WeStCOMS2_lineID.gpkg")
+
+mesh %>% filter(line_id %in% unique(fsa.pt$join_line_id)) %>%
+  st_write("data\\WeStCOMS2_fsaLines.gpkg")
+
+# st_read("data\\WeStCOMS2_fsaPts.gpkg") %>%
+#   group_by(line_id) %>% arrange(distance) %>%
+#   slice_head(n=1) %>%
+#   ungroup %>%
+#   st_write("data\\WeStCOMS2_fsaPts.gpkg", append=F)
+
+mesh.pt <- st_read("data\\WeStCOMS2_fsaPts.gpkg")
+
+site.pts <- fsa.pt %>% group_by(site.id) %>%
+  slice_head(n=1) %>% ungroup %>%
+  select(site.id, join_line_id)
+
+
+
+bind_rows(mesh.pt %>% 
+            select(-distance, -join_fid, -distance) %>%
+            mutate(source="mesh"),
+          site.pts %>% mutate(source="fsa") %>% rename(line_id=join_line_id)) %>%
+  arrange(line_id, source) %>%
+  group_by(line_id) %>%
+  summarise(site.id=first(site.id)) %>%
+  st_cast("LINESTRING") %>%
+  st_transform(4326) %>%
+  mutate(bearing=stplanr::line_bearing(.)) %>%
+  st_drop_geometry %>%
+  select(site.id, bearing) %>%
+  write_csv("data\\site_coast_bearing.csv")
+  # st_write("data\\fsa_to_mesh_lines.gpkg", append=F)
+  
+
+left_join(read_csv("data\\sampling_local.csv"),
+          read_csv("data\\site_coast_bearing.csv"), 
+          by="site.id") %>%
+  write_csv("data\\sampling_local.csv")
+
+left_join(read_csv("data\\sampling_regional.csv"),
+          read_csv("data\\site_coast_bearing.csv"), 
+          by="site.id") %>%
+  write_csv("data\\sampling_regional.csv")
+
+
+
+
+
+# https://github.com/AndriSignorell/DescTools
+
+ConDisPairs <-function(x){
+  
+  # tab is a matrix of counts
+  # Based on code of Michael Friendly and Laura Thompson
+  
+  # slooooow because of 2 nested for clauses O(n^2)
+  # this is NOT faster when implemented with a mapply(...)
+  
+  # Lookin for alternatives in C
+  # http://en.verysource.com/code/1169955_1/kendl2.cpp.html
+  # cor(..., "kendall") is for dimensions better
+  
+  n <- nrow(x)
+  m <- ncol(x)
+  pi.c <- pi.d <- matrix(0, nrow = n, ncol = m)
+  
+  row.x <- row(x)
+  col.x <- col(x)
+  
+  for(i in 1:n){
+    for(j in 1:m){
+      pi.c[i, j] <- sum(x[row.x<i & col.x<j]) + sum(x[row.x>i & col.x>j])
+      pi.d[i, j] <- sum(x[row.x<i & col.x>j]) + sum(x[row.x>i & col.x<j])
+    }
+  }
+  C <- sum(pi.c * x)/2
+  D <- sum(pi.d * x)/2
+  
+  return(list(pi.c = pi.c, pi.d = pi.d, C = C, D = D))
+  
+}
+
+SomersDelta <- function(x,  y = NULL, direction=c("row","column"), conf.level = NA, ...) {
+  
+  if(!is.null(y)) tab <- table(x, y, ...)
+  else tab <- as.table(x)
+  
+  # tab is a matrix of counts
+  x <- ConDisPairs(tab)
+  
+  # use .DoCount
+  #   if(is.na(conf.level)) {
+  #     d.tab <- as.data.frame.table(tab)
+  #     x <- .DoCount(d.tab[,1], d.tab[,2], d.tab[,3])
+  #   } else {
+  #     x <- ConDisPairs(tab)
+  #   }
+  
+  m <- min(dim(tab))
+  n <- sum(tab)
+  switch( match.arg( arg = direction, choices = c("row","column") )
+          , "row" = { ni. <- colSums(tab) }
+          , "column" = { ni. <- rowSums(tab) }
+  )
+  wt <- n^2 - sum(ni.^2)
+  # Asymptotic standard error: sqrt(sigma2)
+  sigma2 <- 4/wt^4 * (sum(tab * (wt*(x$pi.c - x$pi.d) - 2*(x$C-x$D)*(n-ni.))^2))
+  # debug: print(sqrt(sigma2))
+  
+  somers <- (x$C - x$D) / (n * (n-1) /2 - sum(ni. * (ni. - 1) /2 ))
+  
+  pr2 <- 1 - (1 - conf.level)/2
+  ci <- qnorm(pr2) * sqrt(sigma2) * c(-1, 1) + somers
+  
+  if(is.na(conf.level)){
+    result <- somers
+  } else {
+    result <- c(somers = somers,  lwr.ci=max(ci[1], -1), upr.ci=min(ci[2], 1))
+  }
+  
+  return(result)
+  
+}
+
+
+
+
+
+
+
+
+
+
+# ML ----------------------------------------------------------------------
+
+# https://www.r-bloggers.com/2021/04/random-forest-in-r/
+library(randomForest); library(caret)
+
+for(sp in 1:length(species)) {
+  target <- species[sp]
+  target.df <- read_csv(glue("out{sep}dataset_{target}.csv"))
+  
+  train.rf <- target.df %>% filter(year <= 2017) %>%
+    select(N.bloom, 
+           site.id, lon, lat, 
+           ydaySin, ydayCos, fetch,
+           N.ln_1, N.cat_1, 
+           N.ln_2, N.cat_2, 
+           N.lnWt_1, N.lnWt_2, 
+           one_of(covars)) %>%
+    mutate(N.bloom=factor(N.bloom)) %>%
+    as.data.frame()
+  test.rf <- target.df %>% filter(year > 2017) %>%
+    select(N.bloom, 
+           site.id, lon, lat, 
+           ydaySin, ydayCos, fetch,
+           N.ln_1, N.cat_1, 
+           N.ln_2, N.cat_2, 
+           N.lnWt_1, N.lnWt_2, 
+           one_of(covars)) %>%
+    mutate(N.bloom=factor(N.bloom)) %>%
+    as.data.frame()
+  rf <- randomForest(N.bloom ~ ., data=train.rf, 
+                     xtest=test.rf[,-1], ytest=test.rf[,1], 
+                     proximity=T, importance=T, keep.forest=T)
+  p.rf <- predict(rf, test.rf, type="prob")
+  
+}
+
+
+
+hist(treesize(rf),
+     main = "No. of Nodes for the Trees",
+     col = "green")
+varImpPlot(rf,
+           sort = T,
+           n.var = 10,
+           main = "Top 10 - Variable Importance")
+importance(rf)
+
+partialPlot(rf, train.df, temp_L_wk, "1")
