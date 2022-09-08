6fa62b06939b95cc89cbb98e8cfd456286bf9f0f
Sz-Tim <tim.szewczyk@gmail.com>
2022-09-08 08:14:47 +0100

    prior adjustment

--- code/02_HB_initFit.R
+++ code/02_HB_initFit.R
@@ -197,6 +197,7 @@ for(i in 1:length(covariate_sets)) {
         ~c(prior_string("beta(0.1,1)", nlpar=paste0("p", .x), lb=0, ub=1),
            prior_string("normal(0,1)", class="b", nlpar=paste0("b", .x)),
            prior_string("normal(0,.1)", class="sd", nlpar=paste0("b", .x), lb=0),
+           prior_string("normal(0,.1)", class="sd", nlpar=paste0("p", .x), lb=0),
            prior_string("double_exponential(0,0.1)", class="sds", nlpar=paste0("b", .x), lb=0))) %>%
       do.call('c', .),
     map(covar_date, ~prior_string("double_exponential(0,0.1)", class="b", nlpar=paste0("b", .x))) %>% 
